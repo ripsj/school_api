b50f1317f2eb3755a91953ecb444c57f8e7936bb
Jose Luis Ripoll Carrera <joselripollc@gmail.com>
2019-11-07 00:30:02 -0600

    Databases error

--- dacodes/settings.py
+++ dacodes/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = 'dacodes.wsgi.application'
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db) 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
